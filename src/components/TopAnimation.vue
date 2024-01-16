436b25b6b3d5606dc359366403fd84b688184b26
masahiro <mmaassaa0618@gmail.com>
2024-01-16 20:28:27 +0900

    publish

--- src/components/TopAnimation.vue
+++ src/components/TopAnimation.vue
@@ -15,7 +15,7 @@
         <a class="absolute text-white bottom-5 right-8 text-lg font-bold self-start" href="#" @click="nextCard">
           <p class="next-btn text-xl">Next</p>
         </a>
-        <img :key="cards[currentNum].id" class="w-full object-cover" :src="cards[currentNum].photo" alt="Card Image" />
+        <img :key="cards[currentNum].id"  class="w-full object-cover" :src="cards[currentNum].photo" alt="Card Image" />
       </div>
     </div>
   </section>
@@ -34,17 +34,12 @@ const playFowrd = () => {
       duration: 0.7,
       ease: "sine.out",
     },
-    onStart: () => {
+    
+    onComplete: () => {
       currentNum.value++;
       if (currentNum.value >= cards.length) {
         currentNum.value = 0;
       }
-    },
-    onComplete: () => {
-      // currentNum.value++;
-      // if (currentNum.value >= cards.length) {
-      //   currentNum.value = 0;
-      // }
       playreverse();
     },
   });
