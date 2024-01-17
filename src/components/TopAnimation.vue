ce71ce126c89d5bd9893bb38a69685a2e3ec6f89
masahiro <mmaassaa0618@gmail.com>
2024-01-17 22:51:00 +0900

    update

--- src/components/TopAnimation.vue
+++ src/components/TopAnimation.vue
@@ -1,21 +1,26 @@
 <template>
   <section class="max-w-4xl mx-auto px-5">
     <p class="num">{{ cards[currentNum].id + 1 }} / {{ cards.length }}</p>
+    <div class="grid">
+      <button class="text-lg font-bold place-self-end " @click="nextCard">
+        <span class="next text-xl">Next >></span>
+      </button>
+    </div>
     <div class="card grid grid-cols-1 md:grid-cols-2 auto-rows-auto bg-white mx-auto">
-      <div class="card-info grid grid-rows-3 p-4 gap-4">
+      <div class="card-info grid grid-rows-3 p-4 gap-4 order-2 md:order-1">
         <h2 class="text-xl" id="card-info-title">{{ cards[currentNum].title }}</h2>
         <p class="text-sm" id="card-info-desc">{{ cards[currentNum].description }}</p>
         <a class="grid grid-cols-[max-content_20px] items-center self-center justify-self-end transform transition-transform duration-300 ease-in-out" :href="`/works/${cards[currentNum].id}`">
           <p class="detail-btn text-xl">More Detail</p>
         </a>
       </div>
-      <div class="card-photo relative overflow-hidden">
+      <div class="card-photo relative overflow-hidden order-1 md:order-2">
         <div class="mask-1 mask absolute top-0 left-0 transform -translate-y-full h-full w-1/2"></div>
         <div class="mask-2 mask absolute top-0 right-0 transform translate-y-full h-full w-1/2"></div>
-        <button class="absolute text-white bottom-5 right-8 text-lg font-bold self-start" @click="nextCard">
+        <button class="absolute text-white bottom-100 md:bottom-5 right-8 text-lg font-bold self-start" @click="nextCard">
           <span class="next-btn text-xl">Next</span>
         </button>
-        <img @load="onImageLoaded" class="w-full object-cover" :src="cards[currentNum].photo" :key="cards[currentNum].id" :alt="cards[currentNum].title"  />
+        <img @load="onImageLoaded" class="w-full object-cover" :src="cards[currentNum].photo" :key="cards[currentNum].id" :alt="cards[currentNum].title" />
       </div>
     </div>
   </section>
@@ -121,7 +126,7 @@ const nextCard = () => {
   playFowrd();
 };
 const onImageLoaded = () => {
-  setTimeout((()=>playreverse()),100)
+  setTimeout(() => playreverse(), 100);
 };
 </script>
 
@@ -132,8 +137,14 @@ const onImageLoaded = () => {
 }
 .card {
   box-shadow: rgba(255, 255, 255, 0.1) 0px 1px 1px 0px inset, rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-  aspect-ratio : 2/1;
+  aspect-ratio: 2/1;
 }
+@media (max-width: 768px) {
+  .card {
+    aspect-ratio: 1/2;
+  }
+}
+
 .card-info h2 {
   color: var(--dark-green);
   align-items: center;
@@ -144,6 +155,14 @@ const onImageLoaded = () => {
 }
 .card-info a {
   color: var(--dark-green);
+}
+@media (max-width: 768px) {
+  .next-btn {
+    background: black;
+    color: white;
+    display: inline-block;
+    padding: 0 4px;
+  }
 }
 .card-info a:hover {
   transform: translateX(5px);
@@ -158,6 +177,11 @@ const onImageLoaded = () => {
   background: rgba(255, 255, 255, 0.8);
   z-index: 0;
   clip-path: polygon(100% 60%, 60% 100%, 100% 100%);
+}
+@media (max-width: 768px) {
+  .card-photo::before {
+    clip-path: polygon(0, 0, 0, 0, 0, 0);
+  }
 }
 .card-photo button {
   color: var(--white);
