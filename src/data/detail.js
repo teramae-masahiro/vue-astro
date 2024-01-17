2752758687ab30f540262c364be4eec16d6f2341
masahiro <mmaassaa0618@gmail.com>
2024-01-17 19:10:29 +0900

    image

--- src/data/detail.js
+++ src/data/detail.js
@@ -3,35 +3,35 @@ export const worksDetail = [
     id: 0,
     title: "Laviere",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci vulputate, eget tincidunt nisl ultricies.",
-    photo: "../assets/works1.jpg",
+    photo: "assets/works1.jpg",
     url: "https://laviere-salon.co.jp/"
   },
   {
     id: 1,
     title: "Nakayoshi Seven",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci vulputate, eget tincidunt nisl ultricies.",
-    photo: "../assets/works2.jpg",
+    photo: "assets/works2.jpg",
     url: "https://kodomonoie.club"
   },
   {
     id: 2,
     title: "Ueda Sosyoku Bijyutu",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci vulputate, eget tincidunt nisl ultricies.",
-    photo: "../assets/works3.jpg",
+    photo: "assets/works3.jpg",
     url: "https://ueda-sosyoku-bijutsu.com/"
   },
   {
     id: 3,
     title: "Chance Football",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci vulputate, eget tincidunt nisl ultricies.",
-    photo: "../assets/works4.jpg",
+    photo: "assets/works4.jpg",
     url: "https://chance-football.jp/"
   },
   {
     id: 4,
     title: "X-House",
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec nec justo eget felis facilisis fermentum. Aliquam porttitor mauris sit amet orci vulputate, eget tincidunt nisl ultricies.",
-    photo: "../assets/works5.jpg",
+    photo: "assets/works5.jpg",
     url: "https://lp.x-house.co.jp/sharehouse/jp/listing2/"
   },
 ]
