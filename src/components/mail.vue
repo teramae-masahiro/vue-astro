84cc8e1ee256cd808ae77830b68b9b9c2a2588e8
masahiro <mmaassaa0618@gmail.com>
2024-01-18 18:46:35 +0900

    test

--- src/components/mail.vue
+++ src/components/mail.vue
@@ -1,44 +1,73 @@
 <template>
-  <div class="bg-white px-6 py-24 lg:px-8">
+  <div class="isolate bg-white px-6 py-24 sm:py-32 lg:px-8">
     <div class="mx-auto max-w-xl text-center">
-      <h2 class="text-3xl font-bold text-gray-900 sm:text-4xl">お問い合わせ</h2>
-      <p class="mt-2 text-lg text-gray-600">無料相談受付中！</p>
+      <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">お問い合わせ</h2>
+      <p class="mt-2 text-lg leading-8 text-gray-600">無料相談受付中！</p>
     </div>
-    <form @submit.prevent="handleSubmit" method="POST" data-netlify="true" class="mx-auto mt-16 max-w-xl sm:mt-20">
+    <form @submit.prevent="handleSubmit" method="POST" action="/" data-netlify="true" class="mx-auto mt-16 max-w-xl sm:mt-20">
       <div class="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
         <div>
-          <label for="name">社名 / お名前</label>
-          <input type="text" name="name" id="name" v-model="formState.name" />
-          <ul>
-            <li v-for="message in errorMessagesState.name" :key="message">
-              {{ message }}
-            </li>
-          </ul>
+          <label for="name" class="block text-sm font-semibold leading-6 text-gray-900">社名 / お名前</label>
+          <div class="mt-2.5">
+            <input
+              :class="['block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6', { 'formGroup__input-error': errorMessagesState.name.length }]"
+              type="text"
+              name="name"
+              id="name"
+              autocomplete="given-name"
+              v-model="formState.name"
+            />
+            <ul class="formGroup__errorMessages">
+              <li v-for="message in errorMessagesState.name" :key="message">
+                {{ message }}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <label for="email">メールアドレス</label>
-          <input type="email" name="email" id="email" v-model="formState.email" />
-          <ul>
-            <li v-for="message in errorMessagesState.email" :key="message">
-              {{ message }}
-            </li>
-          </ul>
+        <div class="sm:col-span-2">
+          <label for="email" class="block text-sm font-semibold leading-6 text-gray-900">メールアドレス</label>
+          <div class="mt-2.5">
+            <input
+              :class="['block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6', { 'formGroup__input-error': errorMessagesState.email.length }]"
+              type="email"
+              name="email"
+              id="email"
+              autocomplete="email"
+              v-model="formState.email"
+            />
+            <ul class="formGroup__errorMessages">
+              <li v-for="message in errorMessagesState.email" :key="message">
+                {{ message }}
+              </li>
+            </ul>
+          </div>
         </div>
-        <div>
-          <label for="message">お問い合わせ内容</label>
-          <textarea name="message" id="message" rows="4" v-model="formState.message"></textarea>
-          <ul>
-            <li v-for="message in errorMessagesState.message" :key="message">
-              {{ message }}
-            </li>
-          </ul>
+        <div class="sm:col-span-2">
+          <label for="message" class="block text-sm font-semibold leading-6 text-gray-900">お問い合わせ内容</label>
+          <div class="mt-2.5">
+            <textarea
+              :class="['block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6', { 'formGroup__input-error': errorMessagesState.message.length }]"
+              name="message"
+              id="message"
+              rows="4"
+              v-model="formState.message"
+            ></textarea>
+            <ul class="formGroup__errorMessages">
+              <li v-for="message in errorMessagesState.message" :key="message">
+                {{ message }}
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-      <button type="submit">送信する</button>
+      <div class="mt-10">
+        <button type="submit" class="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-black shadow-sm focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">送信する</button>
+      </div>
       <p v-if="submitted">Thank you for your message!</p>
     </form>
   </div>
 </template>
+
 <script setup>
 import { reactive } from "vue";
 
